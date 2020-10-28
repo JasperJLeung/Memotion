8b9c35e482932846d78989b49be09c5b3c26e9ce
Jasper Leung <JasperLeung@Jaspers-MacBook-Pro.local>
2020-10-28 17:40:23 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,100 @@
+let getEmotion = (url) => {
+    var bodyFormData = new FormData();
+    bodyFormData.append("photo", url);
+  
+    axios({
+      method: "post",
+      url: "https://api.luxand.cloud/photo/emotions",
+      data: bodyFormData,
+      headers: {
+        "Content-Type": "multipart/form-data",
+        token: "7d1bde7b82014a5294b0e825231a6c36"
+      }
+    })
+      .then(function (response) {
+        //handle success
+        console.log(response.data);
+        response.data.faces[0].emotions.happiness && beHappy();
+        response.data.faces[0].emotions.sadness && sadnessBeGone();
+        response.data.faces[0].emotions.neutral && beHappier();
+      })
+      .catch(function (response) {
+        //handle error
+        console.log(response);
+      });
+  };
+  
+  // cloudinary api
+  
+  var myWidget = cloudinary.createUploadWidget(
+    {
+      cloudName: "dfjro4g4f",
+      uploadPreset: "aoskj9no",
+      max_files: 1
+    },
+    (error, result) => {
+      if (!error && result && result.event === "success") {
+        getEmotion(result.info.secure_url);
+        document.querySelector("#upload_widget").classList.toggle("display", 1);
+        document.querySelector(
+          ".mood__image"
+        ).innerHTML = `<img src=${result.info.secure_url}>`;
+      }
+    }
+  );
+  
+  document.getElementById("upload_widget").addEventListener(
+    "click",
+    function () {
+      myWidget.open();
+    },
+    false
+  );
+  
+  // memes
+  
+  let sadnessBeGone = () => {
+    axios
+      .get(
+        "https://cors-anywhere.herokuapp.com/https://some-random-api.ml/img/panda"
+      )
+      .then((results) => {
+        document.querySelector(".mood__text").innerHTML =
+          "Uh Oh Someone's Looking Sad :(";
+        document.querySelector(
+          ".mood__meme"
+        ).innerHTML = `<img src=${results.data.link}>`;
+        console.log(results.data.link);
+      });
+  };
+  
+  let beHappy = () => {
+    axios
+      .get(
+        "https://cors-anywhere.herokuapp.com/https://some-random-api.ml/img/cat"
+      )
+      .then((results) => {
+        document.querySelector(".mood__text").innerHTML =
+          "I AM VERY CAT I MEANT HAPPY";
+        document.querySelector(
+          ".mood__meme"
+        ).innerHTML = `<img src=${results.data.link}>`;
+        console.log(results.data.link);
+      });
+  };
+  
+  let beHappier = () => {
+    axios
+      .get(
+        "https://cors-anywhere.herokuapp.com/https://some-random-api.ml/img/dog"
+      )
+      .then((results) => {
+        document.querySelector(".mood__text").innerHTML =
+          "I AM VERY NEUTRAL AND I WANT A DOG";
+        document.querySelector(
+          ".mood__meme"
+        ).innerHTML = `<img src=${results.data.link}>`;
+        console.log(results.data.link);
+      });
+  };
+  
